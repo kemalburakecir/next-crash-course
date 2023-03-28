1737bb259d70c23bc27b7194ea84e76fc45a5eec
Kemal Burak Ecir <kemalburak.ecir@gmail.com>
2023-03-28 08:47:57 -0400

    folders

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
-import Layout from '../components/Layout'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+function MyApp ({Component, pageProps }) {
+    return <Component {...pageProps} />
 }
 
 export default MyApp
+
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,9 @@
-import { server } from '../config'
-import ArticleList from '../components/ArticleList'
+    // home sayfamiz // route page 
 
-export default function Home({ articles }) {
-  return (
-    <div>
-      <ArticleList articles={articles} />
-    </div>
-  )
+import React from "react";
+
+function HomePage () {
+    return <h1> The Homeee Page </h1>
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/articles`)
-  const articles = await res.json()
-
-  return {
-    props: {
-      articles,
-    },
-  }
-}
-
-// export const getStaticProps = async () => {
-//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
-//   const articles = await res.json()
-
-//   return {
-//     props: {
-//       articles,
-//     },
-//   }
-// }
+export default HomePage;
